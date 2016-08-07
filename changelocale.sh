75db951450033de7ef0cffc0cdd2c9469a4584e6
Chandrakant Singh <aryalinux11@gmail.com>
2016-08-07 10:32:31 +0530

    Fixed the locale-not changing issue in 2016.08 where /etc/profile.d/i18n.sh is present

--- changelocale.sh
+++ changelocale.sh
@@ -3,6 +3,16 @@
 cat > /etc/locale.conf <<EOF
 LANG=$1
 EOF
+
+if [ -f /etc/profile.d/i18n.sh ]
+then
+
+cat > /etc/profile.d/i18n.sh << EOF
+# Set up i18n variables
+export LANG=$1
+EOF
+
+fi
 sudo rm /etc/xdg/autostart/languageselector.desktop
 
 sudo killall lightdm
